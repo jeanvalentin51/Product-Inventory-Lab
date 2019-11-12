51bcc731d7f2c67eaac0154ccbf1ea954b2bfa06
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-12 15:54:07 -0500

    Add tests/logic for whiskey class

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -6,5 +6,15 @@ public class Whiskey {
     private int id;
     private int qty;
 
+    public String getBrand() { return this.brand; }
 
+    public void setBrand(String brand) { this.brand = brand; }
+
+    public int getQty() { return this.qty; }
+
+    public void setQty(int qty) { this.qty = qty; }
+
+    public int getId() { return this.id; }
+
+    public void setId(int id) { this.id = id; }
 }
